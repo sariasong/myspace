041157699c73d03273d29b716ac37dd9699ca212
Sarah Daenitz <sarahdaenitz@gmail.com>
2018-03-15 18:37:56 -0600

    terrible

---
+++ db/seeds.rb
@@ -0,0 +1,16 @@
+10.times do
+  user = User.create(
+    name: Faker::Cat.name
+    image: Faker::Avatar.image
+    email: Faker::Internet.unique.email
+    password: 'password'
+  )
+  5.times do
+    user.posts.create(
+      title: Faker::Hipster.word
+      body: Faker::Hipster.sentence
+    )
+  end
+end
+
+puts "seeded"
